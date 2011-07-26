eacdbd3204de0041a60e5357937c8b9fcc514744
Chris Davies <mcd@daviesinc.com>
2011-07-26 12:26:19 -0400

    added py-bcrypt as dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     long_description = 'Pyramid starter project to add Velruse, Flash Messages, CSRF, ReCaptcha and Sessions',
     version= version,
     install_requires=[
+      "py-bcrypt",
       "velruse",
       "pyramid",
       "wtforms",
