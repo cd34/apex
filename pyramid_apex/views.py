4dc53b12273451a905e2cd39a49f6b16848fd745
Chris Davies <mcd@daviesinc.com>
2011-08-09 00:10:49 -0400

    modified to allow exclusion of configured providers

--- pyramid_apex/views.py
+++ pyramid_apex/views.py
@@ -50,7 +50,9 @@ def login(request):
     
     velruse_forms = []
     if apex_settings('velruse_config'):
-        for provider in parse_config_file(apex_settings('velruse_config'))[0].keys():
+        if apex_settings('provider_exclude'):
+            for provider in apex_settings('provider_exclude').split(','):                configs.remove(provider.strip())
+        for provider in configs:
             if provider_forms.has_key(provider):
                 velruse_forms.append(provider_forms[provider](
                     end_point='%s?csrf_token=%s&came_from=%s' % \
