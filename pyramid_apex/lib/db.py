fcedaa968cad9c06633fd2f10e95b5f4d8571adc
Matthew Housden <matthew@matthewhousden.com>
2011-08-18 01:42:18 -0400

    removed true/false from get_or_create

--- pyramid_apex/lib/db.py
+++ pyramid_apex/lib/db.py
@@ -2,9 +2,9 @@
 def get_or_create(session, model, **kw):
     obj = session.query(model).filter_by(**kw).first()
     if obj:
-        return obj, False
+        return obj
     else:
         obj = model(**kw)
         session.add(obj)
         session.flush()
-        return obj, True
+        return obj
