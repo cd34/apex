004aefc7c0912212c9f7a896502a0767fe4652cb
Your Name <mcd@daviesinc.com>
2011-08-20 21:30:05 -0400

    fixed quickstart docs, modified to get unit tests working

--- setup.py
+++ setup.py
@@ -2,19 +2,26 @@ from setuptools import setup, find_packages
 
 version = '0.9.0'
 
+install_requires=[
+  "cryptacular",
+  "velruse",
+  "pyramid",
+  "pyramid_mailer",
+  "wtforms",
+  "wtforms-recaptcha",
+]
+
+tests_require = install_requires + ['Sphinx', 'docutils', 
+                                    'WebTest', 'virtualenv']
+
 setup(
     version = version,
-    name = 'Apex',
-    long_description = 'Pyramid starter project to add Velruse, Flash Messages, CSRF, ReCaptcha and Sessions',
-    install_requires=[
-      "cryptacular",
-      "velruse",
-      "pyramid",
-      "pyramid_mailer",
-      "wtforms",
-      "wtforms-recaptcha",
-    ],
-    packages= find_packages(),
+    name='Apex',
+    long_description='Pyramid starter project to add Velruse, Flash Messages, CSRF, ReCaptcha and Sessions',
+    install_requires=install_requires,
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
+    tests_require=tests_require,
+    test_suite="pyramid.tests",
 )
