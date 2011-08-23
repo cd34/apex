6685db0831342e74504b1403623d2e8795403143
Your Name <mcd@daviesinc.com>
2011-08-22 21:46:24 -0400

    modified test to consolidate

--- apex/tests/test_lib_libapex.py
+++ apex/tests/test_lib_libapex.py
@@ -9,31 +9,24 @@ class Test_lib_libapex(unittest.TestCase):
     def tearDown(self):
         testing.tearDown()
 
-    def test_apexid_from_url_google(self):
+    def test_apexid_from_url(self):
         from apex.lib.libapex import apexid_from_url
 
+        # Test Google
         self.assertEqual('$G$xxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', apexid_from_url('Google','https://www.google.com/accounts/o8/id?id=xxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'))
         self.assertEqual(None, apexid_from_url('Google','https://www.google.com/accounts/o8/id?xx=xxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'))
 
-    def test_apexid_from_url_twitter(self):
-        from apex.lib.libapex import apexid_from_url
-
+        # Test Twitter
         self.assertEqual('$T$xxxxxxxx', apexid_from_url('Twitter',"http://twitter.com/?id=['xxxxxxxx']"))
         self.assertEqual(None, apexid_from_url('Twitter',"http://twitter.com/?xx=['xxxxxxxx']"))
 
-    def test_apexid_from_url_facebook(self):
-        from apex.lib.libapex import apexid_from_url
-
+        # Test Facebook
         self.assertEqual('$F$xxxxxxxxxx', apexid_from_url('Facebook','https://graph.facebook.com/xxxxxxxxxx'))
         self.assertEqual(None, apexid_from_url('Facebook','https://graph.facebook.com'))
 
-    def test_apexid_from_url_yahoo(self):
-        from apex.lib.libapex import apexid_from_url
-
+        # Test Yahoo
         self.assertEqual('$Y$xxxxxxxxxxxxxxxxxx.xxxxx#xxxxx', apexid_from_url('Yahoo','https://me.yahoo.com/a/xxxxxxxxxxxxxxxxxx.xxxxx#xxxxx'))
         self.assertEqual(None, apexid_from_url('Yahoo','https://me.yahoo.com/a'))
 
-    def test_apexid_from_url_invalid_provider(self):
-        from apex.lib.libapex import apexid_from_url
-
+        # Test Invalid Provider name
         self.assertEqual(None, apexid_from_url('Blah','https://www.google.com/accounts/o8/id?id=xxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'))
