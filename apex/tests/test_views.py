43db4bd367170b8c868199bf5b6aace72a614d0f
John Anderson <sontek@gmail.com>
2011-12-06 17:32:39 -0600

    created a better BaseTest to speedup the tests and created some login tests

--- apex/tests/test_views.py
+++ apex/tests/test_views.py
@@ -1,9 +1,10 @@
 import os
 import unittest
 
-from sqlalchemy import engine_from_config
-
 from pyramid import testing
+
+from apex.tests import BaseTestCase
+from paste.util.multidict import MultiDict
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -15,51 +16,56 @@ environ = {
     'SERVER_NAME':'test.com',
 }
 
-""" bare minimum settings required for testing
-"""
-settings = {
-    'sqlalchemy.url':'sqlite:///apex.test.db',
-    'mako.directories':'{0}/../apex/templates'.format(here),
-    'apex.session_secret':'session_secret',
-    'apex.auth_secret':'auth_secret',
-    'apex.came_from_route':'home',
-    'apex.velruse_config':'{0}/CONFIG.yaml',
-}
 
-class Test_views(unittest.TestCase):
-    def setUp(self):
-        """ must add default route 'home' and include apex
-            we also must create a default user/pass/group to test
-        """
-        import apex
+class Test_views(BaseTestCase):
+    def test_view_login(self):
         from apex.lib.libapex import create_user
-        self.engine = engine_from_config(settings, 'sqlalchemy.')
-        self.config = testing.setUp()
-        self.config.add_route('home', '/')
-        self.config.add_settings(settings)
-        self.config.include('apex')
-
-        """ creating user for later testing
         create_user(username='test', password='password')
-        """
 
-    def tearDown(self):
-        """ import Base so that we can drop the tables after each test
-        """
-        from apex.models import Base
-        testing.tearDown()
-        Base.metadata.drop_all(self.engine)
-
-    def test_login(self):
-        pass
-        """ requires REMOTE_ADDR
         from apex.views import login
-        request = testing.DummyRequest(environ=environ)
+        request = testing.DummyRequest()
+
+        # wtforms requires this
+        request.POST = MultiDict()
+
         request.context = testing.DummyResource()
         response = login(request)
 
-        self.assertEqual('302 Found', response.status)
-        """
+        self.assertEqual(response['title'], 'You need to login')
+
+    def test_simple_login(self):
+        from apex.lib.libapex import create_user
+        create_user(username='test', password='password')
+
+        request = testing.DummyRequest(environ=environ)
+        request.method = 'POST'
+        # wtforms requires this
+        request.POST = MultiDict()
+        request.POST['username'] = 'test'
+        request.POST['password'] = 'password'
+
+        from apex.views import login
+        request.context = testing.DummyResource()
+        response = login(request)
+
+        self.assertEqual(response.status_int, 302)
+
+    def test_fail_login(self):
+        from apex.lib.libapex import create_user
+        create_user(username='test', password='password1')
+
+        request = testing.DummyRequest(environ=environ)
+        request.method = 'POST'
+        # wtforms requires this
+        request.POST = MultiDict()
+        request.POST['username'] = 'test'
+        request.POST['password'] = 'password'
+
+        from apex.views import login
+        request.context = testing.DummyResource()
+        response = login(request)
+
+        self.assertEqual(len(response['form'].errors), 1)
 
     def test_logout(self):
         """ need to import environ for SERVER_NAME and HOST_NAME
