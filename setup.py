38972e13a5ed23f95e536f497d8bbd7550c0835b
Patrick Ancillotti <patrick@eefy.net>
2012-07-02 15:55:57 -0600

    Updated setup.py to include some pep8 fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 version = '0.9.5'
 
-install_requires=[
+install_requires = [
     "cryptacular",
     "zope.sqlalchemy",
     "velruse==0.20a1",
@@ -19,32 +19,34 @@ tests_require = install_requires + ['Sphinx', 'docutils',
 kwargs = dict(
     version=version,
     name='Apex',
-    long_description='Pyramid starter project to add Velruse, Flash Messages, CSRF, ReCaptcha and Sessions',
+    long_description="""\
+Pyramid starter project to add Velruse, Flash Messages,\
+CSRF, ReCaptcha and Sessions""",
     install_requires=install_requires,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
     tests_require=tests_require,
     test_suite="apex.tests",
-    entry_points = """\
+    entry_points="""\
         [paste.paster_create_template]
         apex_routesalchemy=apex.scaffolds:ApexRoutesAlchemyTemplate
-    """ 
+    """
 )
 
 # to update catalogs, use babel and lingua !
 try:
     import babel
-    babel = babel # PyFlakes
+    babel = babel  # PyFlakes
     # if babel is installed, advertise message extractors (if we pass
     # this to setup() unconditionally, and babel isn't installed,
     # distutils warns pointlessly)
-    kwargs['message_extractors'] = { ".": [
-        ("**.py",     "lingua_python", None ),
+    kwargs['message_extractors'] = {".": [
+        ("**.py",     "lingua_python", None),
         ('**.mako', 'mako', None),
-        ("**.pt", "lingua_xml", None ),
-        ]}
+        ("**.pt", "lingua_xml", None), ]
+    }
 except ImportError:
-    pass  
+    pass
 
 setup(**kwargs)
