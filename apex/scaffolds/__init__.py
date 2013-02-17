b73fbfc341a98e484478297ba4e68d3f01d5bef5
Josip Delic <delijati@googlemail.com>
2013-02-17 15:40:44 +0100

    fixed tests use create_user from lib

--- apex/scaffolds/__init__.py
+++ apex/scaffolds/__init__.py
@@ -1,7 +1,6 @@
-from paste.util.template import paste_script_template_renderer
 from pyramid.scaffolds import PyramidTemplate
+
 
 class ApexRoutesAlchemyTemplate(PyramidTemplate):
     _template_dir = 'apex_routesalchemy'
     summary = 'Pyramid SQLAlchemy project using url dispatch, and apex'
-    template_renderer = staticmethod(paste_script_template_renderer)
